dc0a8cdfe5033b8b39ddcd950af71c97210b8e42
macas99 <marvndude@gmail.com>
2022-10-02 15:26:23 +0200

    Small css changes

--- frontend/notes-frontend/src/components/Note.js
+++ frontend/notes-frontend/src/components/Note.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Note(props) {
   return (
-    <div className="animate__animated animate__bounceIn note">
+    <div className="animate__animated animate__bounceIn noselect note">
       <h1>{props.title}</h1>
       <p>{props.content}</p>
     </div>
