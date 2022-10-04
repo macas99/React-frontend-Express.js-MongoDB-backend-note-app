9d74a405428a2c0e656ca4d5ba7da421a21dedfd
macas99 <marvndude@gmail.com>
2022-10-04 14:25:12 +0200

    Note modal css and submit function

--- frontend/notes-frontend/src/components/NoteModal.js
+++ frontend/notes-frontend/src/components/NoteModal.js
@@ -1,18 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
+import CloseIcon from '@mui/icons-material/Close';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
 
 function NoteModal(props) {
+  const [note, setNote] = useState({
+    title: props.title,
+    content: props.content
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote(prevNote => {
+      return {
+        ...prevNote,
+        [name]: value
+      };
+    });
+  }
 
   function handleClickOutside(event) {
-    if(event.target.classList.contains('modal-container')){
+    if (event.target.classList.contains('modal-container')) {
       props.closeModal();
     }
   }
-  
+
+  function onFormSubmit(event) {
+    event.preventDefault();
+
+    const editedNote = {
+      ...note,
+      title: document.getElementById("myContentEditable").innerHTML
+    }
+
+    console.log(editedNote);
+    props.closeModal();
+  }
+
   return (
     <div className='modal-container' onClick={handleClickOutside}>
       <div className='note-modal'>
-        <h1>{props.title}</h1>
-        <p>{props.content}</p>
+        <div className="text-end" >
+          <CloseIcon className="close-modal-btn" onClick={props.closeModal} />
+        </div>
+
+        <form name="search-form" id="search-form" className="modal-input" onSubmit={onFormSubmit}>
+
+          <p name="title" id="myContentEditable" contentEditable="true" suppressContentEditableWarning={true}>{note.title}</p>
+
+          <textarea
+            className="input-content"
+            name="content"
+            value={note.content}
+            placeholder="Take a note..."
+            onChange={handleChange}
+          />
+
+          <div className="text-end">
+            <button type="submit">
+              <AddCircleIcon />
+            </button>
+          </div>
+        </form>
+
       </div>
     </div>
   );
